e3ab6ea57c418f8b5568575d1afc188b5292659f
O Ko TuTuUTa <shit141414@gmail.com>
2024-12-12 23:46:01 +0800

    test3

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ import json
 
 app = Flask(__name__)
 
-src="https://od.moi.gov.tw/api/v1/rest/datastore/A01010000C-001277-053"
-with request.urlopen(src) as response:
-    data = json.load(response)
+# src="https://od.moi.gov.tw/api/v1/rest/datastore/A01010000C-001277-053"
+# with request.urlopen(src) as response:
+#     data = json.load(response)
+
+# @app.route('/')
+# def index():
+#     return render_template("fraud_list.html", data = data)
 
 @app.route('/')
 def index():
-    return render_template("fraud_list.html", data = data)
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run()
